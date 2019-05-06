0f11db67c6342eff6794a58ff749a37679735fb2
Benni11833 <benedikt.happe@alumni.fh-aachen.de>
2019-05-06 20:16:25 +0200

    update2

---
+++ Tree.h
@@ -0,0 +1,37 @@
+/*************************************************
+* ADS Praktikum 2.2
+* Tree.h
+* Erweiterung um Hilfsattribute und -funktionen gestattet, wenn erforderlich.
+*************************************************/
+#pragma once
+#include <string>
+#include "TreeNode.h"
+#include "catch.h"
+
+using namespace std;
+
+
+class Tree {
+private:
+	TreeNode * anker;
+	int NodeIDCounter;
+	bool rec_searchNode(TreeNode* tmp, std::string Name);
+	TreeNode* searchNode(int NodePosID);
+	bool isLeaf(TreeNode*);
+	bool isRoot(TreeNode*);
+	bool has2Child(TreeNode*);
+	bool has1Child(TreeNode*);
+	TreeNode* Min(TreeNode*);
+	void print_preorder(TreeNode*);
+	TreeNode* getParent(TreeNode *);
+	bool balanceTree(void);
+	void printLevelOrder(void);
+	bool rotateTreeRight(TreeNode*, TreeNode*);
+	bool rotateTreeLeft(TreeNode*, TreeNode*);
+public:
+	Tree();
+	void addNode(std::string Name, int Alter, double Einkommen, int PLZ);
+	bool searchNode(std::string Name);
+	// friend-Funktionen sind f�r die Tests erforderlich und m�ssen unangetastet bleiben! 
+	friend TreeNode * get_anker(Tree& TN);
+};
---
+++ TreeNode.h
@@ -0,0 +1,46 @@
+/*************************************************
+* ADS Praktikum 2.2
+* TreeNode.h
+* Erweiterung um Hilfsattribute und -funktionen gestattet, wenn erforderlich.
+*************************************************/
+#pragma once
+#include <string>
+#include <iomanip>
+
+#define setw_width 5
+
+using namespace std;
+
+class TreeNode{
+	
+	private:
+		int NodePosID;
+		int NodeID;
+		std::string Name;
+		int Alter;
+		double Einkommen;
+		int PLZ;
+		TreeNode* left, *right;
+		bool red;
+	public:
+		TreeNode(int NodePosID, int NodeID, std::string Name, 
+						int Alter, double Einkommen, int PLZ);
+		//Getter-/Setter
+		int getNodePosID();
+		int getNodeID();
+		std::string getName();
+		void setName(std::string);
+		int getAlter();
+		void setAlter(int);
+		double getEinkommen();
+		void setEinkommen(double);
+		int getPLZ();
+		void setPLZ(int);
+		void setRed(bool);
+		bool getRed()const;
+		TreeNode* getLeft();
+		TreeNode* getRight();
+		void setLeft(TreeNode*);
+		void setRight(TreeNode*);
+		void print();
+};
